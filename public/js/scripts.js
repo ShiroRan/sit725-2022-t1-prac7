7fee2338164644529c7104bb5c6f94d86e2d59bc
ZHEHAO ZHOU <zhouzheh@deakin.edu.au>
2022-04-11 20:57:53 +1000

    change to use the template web page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,17 +1,40 @@
-const cardList = [
-
-]
 const clickMe = () => {
     alert("Thanks for clicking me. Hope you have a nice day!")
 }
+
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
-
+    formData.title = $('#title').val();
+    formData.image = $('#image').val();
+    formData.link = $('#link').val();
+    formData.description = $('#description').val();
     console.log("Form Data Submitted: ", formData);
+    addProjectToApp(formData);
+}
+
+//ADD projects to database and reload data
+const addProjectToApp = (project) => {
+    $.ajax({
+        url: '/api/projects',
+        data: project,
+        type: 'POST',
+        success: (result) => {
+            alert(result.message);
+            location.reload();
+        }
+    })
+}
+//GET Projects from database
+const getProjects = () => {
+    $.get('/api/projects',(response) => {
+        if(response.statusCode==200){
+            console.log(response)
+            addCards(response.data);
+        }
+        else {
+            console.log(response)
+        }
+    })
 }
 
 const addCards = (items) => {
@@ -22,7 +45,7 @@ const addCards = (items) => {
     '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
     '<div class="card-reveal">'+
         '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text grey-text text-darken-4">'+item.desciption+'</p>'+
+        '<p class="card-text grey-text text-darken-4">'+item.description+'</p>'+
       '</div></div></div>';
       $("#card-section").append(itemToAppend)
     });
@@ -35,6 +58,6 @@ $(document).ready(function(){
     $('#formSubmit').click(()=>{
         submitForm();
     })
-    addCards(cardList);
+    getProjects();
     $('.modal').modal();
   });
